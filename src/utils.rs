69463b70c7100a871efa4ec5ab1d369e431f9f4a
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2024-10-12 08:29:07 +1000

    prepared file moves

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,18 @@
+use crate::equipment::EquipmentType;
+
 pub fn string_to_option(s: String) -> Option<String> {
 	if s.is_empty() {
 		None
 	} else {
 		Some(s)
 	}
+}
+
+pub fn get_equipment_folder_name(equipment_type: EquipmentType, id: i32) -> String {
+	let equipment_type_short = match equipment_type {
+		EquipmentType::Flask => "F",
+		EquipmentType::Vessel => "V",
+		EquipmentType::IncubationCabinet => "I",
+	};
+	format!("{equipment_type_short}-{id}/")
 }
