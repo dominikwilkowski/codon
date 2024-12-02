208285e90823866e859d0e574a0bfb7a00ab6b71
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2024-12-03 06:37:13 +1000

    notes for profile

--- src/profile/profile_view.rs
+++ src/profile/profile_view.rs
@@ -4,5 +4,26 @@ stylance::import_style!(css, "profile.module.css");
 
 #[component]
 pub fn Profile() -> impl IntoView {
-	view! { <div>Your profile</div> }
+	view! {
+		<div>
+			<h1>Your profile</h1>
+			- username
+			- password
+			- employee_id
+			- status
+			- first_name
+			- last_name
+			- preferred_name
+			- email
+			- phone_number
+			- department
+			- role
+			- hire_date
+			- emergency_contact
+			- certifications
+			- specializations
+			- picture
+			- bio
+		</div>
+	}
 }
